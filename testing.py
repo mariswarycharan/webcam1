5bee0b74057dc9922afc3d225dd69317cc5cfd16
Charan A A <104826220+mariswarycharan@users.noreply.github.com>
2023-07-16 20:24:22 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,5 +1,17 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 
+def check_socket_connection():
+    try:
+        socket.create_connection(("localhost", 8080), 2)
+        return True
+    except socket.error:
+        return False
+
+b = st.button("check")
+if b:
+    st.code(check_socket_connection())
+    
+
 webrtc_ctx = webrtc_streamer(
     key="WYH")
