081f9c3f89be20abe29a62b9dd99df5231cbe98e
Charan A A <104826220+mariswarycharan@users.noreply.github.com>
2023-07-25 15:21:11 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,9 +1,35 @@
+# import required libraries
+from vidgear.gears import CamGear
+import cv2
 import streamlit as st
-from webcam import webcam
 
-captured_image = webcam()
-if captured_image is None:
-    st.write("Waiting for capture...")
-else:
-    st.write("Got an image from the webcam:")
-    st.image(captured_image)
+# set desired quality as 720p
+options = {"STREAM_RESOLUTION": "720p"}
+
+# Add any desire Video URL as input source
+# for e.g https://vimeo.com/151666798
+# and enable Stream Mode (`stream_mode = True`)
+stream = CamGear(
+    source=0,
+    stream_mode=False,
+    logging=True,
+
+).start()
+ii = st.image([])
+# loop over
+while True:
+
+    # read frames from stream
+    frame = stream.read()
+
+    # check for frame if Nonetype
+    if frame is None:
+        break
+
+    # {do something with the frame here}
+
+    # Show output window
+    ii.image(frame)
+
+# safely close video stream
+stream.stop()
